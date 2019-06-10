520979b0debc16da22efc0c9ed7c4df7b431c540
Aleksander Chromik <achromik@gmail.com>
2019-06-10 12:05:01 +0200

    Add navigation

---
+++ frontend/src/common/Navigation/Navigation.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+//@ts-ignore
+import styled from '@emotion/styled/macro'
+import { Link } from 'react-router-dom'
+
+import { colors } from '@src/variables'
+import { navItems } from '@src/data/config'
+
+const NavBar = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 4rem;
+    background-color: ${colors.navbarBackgroundColor};
+    padding: 0 4rem;
+`
+
+const NavItem = styled.li`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    list-style: none;
+    flex: 0 1 auto;
+    width: 8rem;
+    height: 100%;
+`
+
+const NavLink = styled(Link)`
+    text-decoration: none;
+    color: ${colors.textColor};
+    & ${NavItem} {
+        &:hover {
+            background-color: ${colors.hoverNavbarBgColor};
+            color: ${colors.secondaryTextColor};
+        }
+    }
+`
+
+interface NavItem {
+    id: number
+    name: string
+    path: string
+}
+
+export const Navigation: React.FC = () => (
+    <NavBar>
+        {navItems.map(
+            ({ id, name, path }: NavItem): React.ReactNode => (
+                <NavLink key={id} to={path}>
+                    <NavItem>{name}</NavItem>
+                </NavLink>
+            ),
+        )}
+    </NavBar>
+)
