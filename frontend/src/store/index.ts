ea51a735ae58cd47027ecff18bcc4003b2009117
Aleksander Chromik <achromik@gmail.com>
2019-06-11 07:35:56 +0200

    set up app's store

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createStore, useLocalStore } from 'global-hook-store'
+
+import { Question } from '@src/common/models'
+
+interface Store {
+    questions: Question[]
+    appState: string
+}
+
+export const store = createStore({ questions: [], appState: '' } as Store, {
+    addQuestion: (store, payload: Question) => ({ ...store, questions: [...store.questions, payload] }),
+})
